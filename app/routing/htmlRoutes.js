84c9581b7beaa9a8c63c6094b4a07a39645078c3
jon <jcrossguitar@gmail.com>
2019-09-16 00:38:27 -0700

    saved documents

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,4 @@
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
+
   
   app.get("/../../survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../../survey.html"));
