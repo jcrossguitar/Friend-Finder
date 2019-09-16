60c86aa0c396bfc60e5498cb451eed32e4e8f342
jon <jcrossguitar@gmail.com>
2019-09-16 00:51:40 -0700

    This commit changed over a few things. I am displaying friends in json data, but having trouble accessing other parts of the website

--- server.js
+++ server.js
@@ -14,25 +14,13 @@ app.use(express.json());
 
 var characters = [
     {
-      routeName: "yoda",
-      name: "Yoda",
-      role: "Jedi Master",
-      age: 900,
-      forcePoints: 2000
-    },
-    {
-      routeName: "darthmaul",
-      name: "Darth Maul",
-      role: "Sith Lord",
-      age: 200,
-      forcePoints: 1200
-    },
-    {
-      routeName: "obiwankenobi",
-      name: "Obi Wan Kenobi",
-      role: "Jedi Master",
-      age: 55,
-      forcePoints: 1350
+      routeName: "Jon",
+      name: "Jon",
+      position: "ER Patient Access Services/Student",
+      sex: "Male",
+      age: 30,
+      city: "San Diego",
+
     }
   ];
 
